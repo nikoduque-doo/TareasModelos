4befd24625ea7fd431fd59058808b95ff37ac661
nikoduque-doo <98504134+nikoduque-doo@users.noreply.github.com>
2024-11-18 16:55:55 -0500

    Intial AjusteDatos.jil

---
+++ AjusteDatos/AjusteDatos.jl
@@ -0,0 +1,13 @@
+### A Pluto.jl notebook ###
+# v0.20.3
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ f03955a0-a5f3-11ef-1c1b-3ffc12415778
+md"""
+# Actividad ajuste de datos
+"""
+
+# ╔═╡ Cell order:
+# ╠═f03955a0-a5f3-11ef-1c1b-3ffc12415778
